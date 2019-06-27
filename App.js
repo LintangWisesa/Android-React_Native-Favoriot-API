52ebfd00795552423b6f81289d49884047fbd95f
lintangwisesa <lintangwisesa@ymail.com>
2019-06-27 09:09:51 +0700

    react native favoriot

--- App.js
+++ App.js
@@ -1,49 +1,83 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, { Component } from 'react'
+import {Alert, Text, View, TextInput, TouchableOpacity } from 'react-native'
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
+class App extends Component {
+constructor(){
+  super();
+  this.state = {
+    dataku: [],
+  };
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+klikPost = () => {
+  Alert.alert('Klik POST')
+} 
+
+klikGet = () => {
+  Alert.alert('Klik GET')
+} 
+
+render() {
+  return (
+  <View style={{backgroundColor:'#febfffa1', flex:1}} >
+    <View style={{flexDirection:'column', alignItems:'center'}}>
+
+    <Text style={{marginTop:20, fontSize:25, fontWeight:'bold' }}>
+    React Native ♥ Favoriot
+    </Text>
+
+    <TextInput
+    placeholder='Insert data KEY here...'
+    style={{height: 55, width: 350, fontSize: 15}}
+    onChangeText={(input1) => this.setState({input1})}
+    value={this.state.input1}
+    />
+
+    <TextInput
+    placeholder='Insert data VALUE here...'
+    style={{height: 55, width: 350, fontSize: 15}}
+    onChangeText={(input2) => this.setState({input2})}
+    value={this.state.input2}
+    />
+    </View>
+
+    <View style={{flexDirection:'row', justifyContent:'center'}}>
+      <TouchableOpacity
+      style={{
+          backgroundColor:'#c33eb2ff', borderRadius:10,
+          flex:1, width:100, height:50, margin:20,
+          flexDirection:'row', justifyContent:'center',
+          alignItems:'center'
+          }}
+      onPress={this.klikPost}
+      >
+      <Text style={{fontSize:20,color:'white',fontWeight:'bold'}}>
+      POST
+      </Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+      style={{
+          backgroundColor:'#c33eb2ff', borderRadius:10,
+          flex:1, width:100, height:50, margin:20,
+          flexDirection:'row', justifyContent:'center',
+          alignItems:'center'
+          }}
+      onPress={this.klikGet}
+      >
+      <Text style={{fontSize:20,color:'white',fontWeight:'bold'}}>
+      GET
+      </Text>
+      </TouchableOpacity>
+    </View>
+
+    <View style={{flexDirection:'column',alignItems:'center'}}>
+    {/* // {data} */}
+    </View>
+
+  </View>
+);
+}
+}
+
+export default App;
