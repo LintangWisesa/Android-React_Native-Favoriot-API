e99f6a13e81fbc7c669497f453c18fe809b38969
lintangwisesa <lintangwisesa@ymail.com>
2019-07-10 15:04:46 +0700

    react native favoriot

--- App.js
+++ App.js
@@ -1,192 +1,193 @@
-import React, { Component } from 'react'
-import {Alert, Text, View, TextInput, TouchableOpacity, ActivityIndicator, FlatList } from 'react-native'
-import axios from 'axios'
+import React, { Component } from 'react';
+import { FlatList, ActivityIndicator, Alert, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import axios from 'axios' // npm i axios
 
-class App extends Component {
+export default class Favoriot extends Component {
+  
   constructor(){
-    super();
+    super()
     this.state = {
-      dataku: [],
+      mydata: [],
       isLoading: false
-    };
+    }
   }
 
-  klikPost = () => {
+  // when GET button clicked
+  getButton = () => {
     this.setState({
       isLoading: true
     })
-    var url = 'https://api.favoriot.com/v1/streams';
-    var header = {
+    var url = 'https://api.favoriot.com/v1/streams?device_developer_id=deviceDefault@Lintang_Wisesa&max=10'
+    var headers = {
+      headers: {
+        'Content-Type': 'application/json',
+        'apikey': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IkxpbnRhbmdfV2lzZXNhIiwicmVhZF93cml0ZSI6dHJ1ZSwiaWF0IjoxNDkzODgyODczfQ.0n_FIr4vapSjewJE2e7cb-FTXs3JsUMTHsTgT2mYNFs'
+      }
+    }
+    axios.get(url, headers)
+    .then((data)=>{
+      this.setState({
+        mydata: data.data.results,  // try the favoriot API first!
+        isLoading: false
+      })
+    })
+    .catch(()=>{
+      this.setState({
+        isLoading: false
+      })
+      Alert.alert('Failed to GET data 😭')
+    })
+  }
+  
+  // when POST button clicked
+  postButton = () => {
+    this.setState({
+      isLoading: true
+    })
+    var url = 'https://api.favoriot.com/v1/streams'
+    var headers = {
       headers: {
         'Content-Type': 'application/json',
         'apikey': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IkxpbnRhbmdfV2lzZXNhIiwicmVhZF93cml0ZSI6dHJ1ZSwiaWF0IjoxNDkzODgyODczfQ.0n_FIr4vapSjewJE2e7cb-FTXs3JsUMTHsTgT2mYNFs'
       }
     }
     var dataBody = {
-      device_developer_id: "deviceDefault@Lintang_Wisesa",
+      device_developer_id: 'deviceDefault@Lintang_Wisesa',
       data: {
         Temperature: this.state.input1,
         Humidity: this.state.input2,
-        Potentio: this.state.input3,
+        Potentio: this.state.input3
       }
     }
-    axios.post(url, dataBody, header)
-    .then((ambilData) => {
-      this.setState({
-        // dataku: ambilData.data.results,
-        isLoading: false
-      })
-      Alert.alert(
-        'Success POST data to Favoriot!'
-      )
-      this.klikGet()
-    })
-    .catch(()=>{
+    axios.post(url, dataBody, headers)
+    .then((data)=>{
       this.setState({
         isLoading: false
       })
-      Alert.alert(
-        'Failed to POST data to Favoriot.\nCheck your connection & try again.'
-      )
+      Alert.alert('Successfully POST the data! 👍')
     })
-  } 
-
-  klikGet = () => {
-    this.setState({
-      isLoading: true
+    .catch(() => {
+      Alert.alert('Failed to POST 😭')
     })
-    var url = 'https://api.favoriot.com/v1/streams?device_developer_id=deviceDefault@Lintang_Wisesa&max=10';
-    axios.get(url, {
-      headers: {
-        'Content-Type': 'application/json',
-        'apikey': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IkxpbnRhbmdfV2lzZXNhIiwicmVhZF93cml0ZSI6dHJ1ZSwiaWF0IjoxNDkzODgyODczfQ.0n_FIr4vapSjewJE2e7cb-FTXs3JsUMTHsTgT2mYNFs'
-      }
-    })
-    .then((ambilData) => {
-      this.setState({
-        dataku: ambilData.data.results,
-        isLoading: false
-      }) 
-    })
-    .catch(()=>{
-      this.setState({
-        isLoading: false
-      })
-      Alert.alert('Failed to GET data from Favoriot.\nCheck your connection & try again.')
-    })
-  } 
+  }
 
   render() {
-
     return (
-      <View style={{backgroundColor:'#febfffa1', flex:1}} >
-        <View style={{flexDirection:'column', alignItems:'center', marginVertical: 10}}>
-
-          <Text style={{marginTop:20, fontSize:25, fontWeight:'bold' }}>
-          React Native ♥ Favoriot
-          </Text>
+      <View style={{ flex: 1, backgroundColor: '#febfffa1' }}>
         
+        {/* title */}
+        <View style={{ flexDirection: 'column', alignItems:'center', marginVertical:10 }}>
+          <Text style={{marginTop:20, fontSize:25, fontWeight:'bold'}}>
+            React Native ♥ Favoriot
+          </Text>
         </View>
 
+        {/* 3 textinputs */}
+        <View style={{flexDirection:'row', justifyContent:'center'}}>
+          
+          <TextInput
+            placeholder='Temperature...'
+            style={{height:55, width:120, fontSize:15, marginHorizontal:8}}
+            keyboardType = 'number-pad'
+            underlineColorAndroid = '#c33eb2ff'
+            value = {this.state.input1}
+            onChangeText = {(input1) => this.setState({input1})}
+          />
+
+          <TextInput
+            placeholder='Humidity...'
+            style={{height:55, width:120, fontSize:15, marginHorizontal:8}}
+            keyboardType = 'number-pad'
+            underlineColorAndroid = '#c33eb2ff'
+            value = {this.state.input2}
+            onChangeText = {(input2) => this.setState({input2})}
+          />
+
+          <TextInput
+            placeholder='Potentio...'
+            style={{height:55, width:120, fontSize:15, marginHorizontal:8}}
+            keyboardType = 'number-pad'
+            underlineColorAndroid = '#c33eb2ff'
+            value = {this.state.input3}
+            onChangeText = {(input3) => this.setState({input3})}
+          />
+
+        </View>
+
+        {/* 2 buttons: GET & POST */}
         <View style={{flexDirection:'row', justifyContent:'center'}}>
 
-          <TextInput
-          placeholder='Temperature ...'
-          style={{height: 55, width: 120, fontSize: 15, marginHorizontal: 8}}
-          onChangeText={(input1) => this.setState({input1})}
-          value={this.state.input1}
-          underlineColorAndroid="#c33eb2ff"
-          keyboardType='number-pad'
-          />
-
-          <TextInput
-          placeholder='Humidity ...'
-          style={{height: 55, width: 120, fontSize: 15, marginHorizontal: 8}}
-          onChangeText={(input2) => this.setState({input2})}
-          value={this.state.input2}
-          underlineColorAndroid="#c33eb2ff"
-          keyboardType='number-pad'
-          />
-
-          <TextInput
-          placeholder='Potentio ...'
-          style={{height: 55, width: 120, fontSize: 15, marginHorizontal: 8}}
-          onChangeText={(input3) => this.setState({input3})}
-          value={this.state.input3}
-          underlineColorAndroid="#c33eb2ff"
-          keyboardType='number-pad'
-          />
-
-        </View>
-
-        <View style={{flexDirection:'row', justifyContent:'center'}}>
-          <TouchableOpacity
-          style={{
-              backgroundColor:'#c33eb2ff', borderRadius:10,
-              flex:1, width:100, height:50, margin:20,
-              flexDirection:'row', justifyContent:'center',
-              alignItems:'center'
-              }}
-          onPress={this.klikPost}
+          {/* POST button */}
+          <TouchableOpacity style={{
+            backgroundColor: '#c33eb2ff', borderRadius:10,
+            flex:1, width:100, height:50, margin:20, flexDirection:'row',
+            justifyContent:'center', alignItems:'center'
+          }}
+          onPress = {this.postButton}
           >
-          <Text style={{fontSize:20,color:'white',fontWeight:'bold'}}>
-            POST
-          </Text>
+            <Text style={{fontSize:20, color:'white', fontWeight:'bold'}}>
+              POST
+            </Text>
+          </TouchableOpacity>
+          
+          {/* GET button */}
+          <TouchableOpacity style={{
+            backgroundColor: '#c33eb2ff', borderRadius:10,
+            flex:1, width:100, height:50, margin:20, flexDirection:'row',
+            justifyContent:'center', alignItems:'center'
+          }}
+          onPress = {this.getButton}
+          >
+            <Text style={{fontSize:20, color:'white', fontWeight:'bold'}}>
+              GET
+            </Text>
           </TouchableOpacity>
 
-          <TouchableOpacity
-          style={{
-              backgroundColor:'#c33eb2ff', borderRadius:10,
-              flex:1, width:100, height:50, margin:20,
-              flexDirection:'row', justifyContent:'center',
-              alignItems:'center'
-              }}
-          onPress={this.klikGet}
-          >
-          <Text style={{fontSize:20,color:'white',fontWeight:'bold'}}>
-            GET
-          </Text>
-          </TouchableOpacity>
         </View>
-
+        
         <View style={{flexDirection:'column', alignItems:'center'}}>
           {
             this.state.isLoading ?
-            <ActivityIndicator style={{margin: 20}} size="large" color="#c33eb2ff" /> :
-            <Text></Text>
-          }
-          
-          {
-            this.state.dataku ?
-            <FlatList
-              data={this.state.dataku}
-              renderItem={
-                ({item, index}) => 
-                <View style={{flexDirection:'row', justifyContent:'center'}}>
-                  <Text style={{fontSize: 16, marginHorizontal: 5}}>
-                    ⏰ {item.stream_created_at.split('T')[0]}
-                  </Text>
-                  <Text style={{fontSize: 16, marginHorizontal: 5}}>
-                    🌡 {item.data.Temperature} °C
-                  </Text>
-                  <Text style={{fontSize: 16, marginHorizontal: 5}}>
-                    💧 {item.data.Humidity} %
-                  </Text>
-                  <Text style={{fontSize: 16, marginHorizontal: 5}}>
-                    🕹 {item.data.Potentio}
-                  </Text>
-                </View>
-                }
-              keyExtractor={(item, index) => index.toString()}
+            <ActivityIndicator
+              style={{margin:20}}
+              size='large'
+              color='#c33eb2ff'
             /> :
             <Text></Text>
           }
-          
+
+          {
+            this.state.mydata ?
+            <FlatList
+              data = {this.state.mydata}
+              renderItem = {
+                ({item, index}) =>
+                  <View style={{flexDirection:'row', justifyContent:'center'}}>
+                    {/* time */}
+                    <Text style={{fontSize:16, marginHorizontal: 5}}>
+                      ⏰ {item.stream_created_at.split('T')[0]}
+                    </Text>
+                    {/* temperature */}
+                    <Text style={{fontSize:16, marginHorizontal: 5}}>
+                      🌡 {item.data.Temperature} °C
+                    </Text>
+                    {/* humidity */}
+                    <Text style={{fontSize:16, marginHorizontal: 5}}>
+                      💧 {item.data.Humidity} %
+                    </Text>
+                    {/* potentio */}
+                    <Text style={{fontSize:16, marginHorizontal: 5}}>
+                      🕹 {item.data.Potentio}
+                    </Text>
+                  </View>
+                }
+              keyExtractor = {(item, index)=>index.toString()}
+            /> :
+            <Text></Text>
+          }
         </View>
 
       </View>
     );
   }
 }
-
-export default App;
